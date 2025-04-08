aae24f2425671214391471a7626c83f65b27fca5
ad <ad@WIN-T85DL6QH581>
2025-04-08 15:33:42 +0400

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Drawing;
 namespace TableMed
 {
     public partial class MainWindow : Window
@@ -19,6 +21,31 @@ namespace TableMed
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            if(dlg.ShowDialog()==true && !string.IsNullOrWhiteSpace(dlg.FileName))
+            {
+                TableM.ItemsSource = dlg.FileName;
+            }
+        }
+        private void BirthDate_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"\d{2}/./\d{2}/./\d{4}");
+            string date=BirthDate.Text;
+            if (!regex.IsMatch(date))
+            {
+                BirthDate.BorderBrush=Brushes.Red;
+            }
         }
     }
 }
